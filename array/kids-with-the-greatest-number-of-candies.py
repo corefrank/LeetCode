fedf7374ef4c5d93b46837a62ff2deba64c931a8
Nan An <93599538+corefrank@users.noreply.github.com>
2026-04-13 12:12:00 +0800

    LC-kids-with-the-greatest-number-of-candies [Runtime: 0 ms; Memory: 19.1 MB]

--- array/kids-with-the-greatest-number-of-candies.py
+++ array/kids-with-the-greatest-number-of-candies.py
@@ -1,8 +1,12 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         nums = len(candies)
-   
-        for i in range nums:
-            if candies[i] + extraCandies >= max(candies):
-                return true
-            else return false
+        greatest = max(candies)
+        result = []# !!!!
+        for i in range(nums):
+            if candies[i] + extraCandies >= greatest:
+                result.append(True) #Python 里的布尔值，必须是首字母大写，JSON（LeetCode 用来展示结果）会是小写
+            else:
+                result.append(False)
+        
+        return result# if  not , the output is [null]
