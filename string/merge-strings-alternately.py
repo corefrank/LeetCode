3dffff49299a1b8fc2c1cc6146866493454d93f5
Nan An <93599538+corefrank@users.noreply.github.com>
2026-04-17 19:12:05 +0800

    LC-merge-strings-alternately [Runtime: N/A; Memory: N/A]

---
+++ string/merge-strings-alternately.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        len1 = len(word1)
+        len2 = len(word2)
+        i = 0
+        j = 0
+        merged = []
+        while i < len1 or j < len2:
+            if i <len1:
+                result += word[i]
+                i += 1
+            if j < lens2:
+                result += word[j]
+                j += 1
+
+        return ''.join(result)
+        
+        
