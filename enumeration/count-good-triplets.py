a8ded4ce3f44981b96cd973651dc753d3d7f67f3
Nan An <93599538+corefrank@users.noreply.github.com>
2026-04-24 18:31:10 +0800

    LC-count-good-triplets [Runtime: N/A; Memory: N/A]

--- enumeration/count-good-triplets.py
+++ enumeration/count-good-triplets.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(len(arr)):
             for j in range(len(arr)):
                 for k in range(len(arr)):
-                    if if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                    if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
                         ans += 1
         return ans
 
